348f517440c9a67f5e9eca54b903386e47b09c71
Moosa Baig <am.moosabaig@gmail.com>
2026-09-03 22:39:21 +0500

    Completed

---
+++ Lab 3/Task6/Task6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+
+
+    printf("Welcome to C programming \n \n \n");
+    printf("Name: \t Ali \n");
+    printf("Age: \t 20 \n");
+    printf("Grade: \t A \n \n \n");
+    printf("File Path: C:\\Users\\Student \n \n \n");
+    printf("He said: \"Hello\"");
+}
