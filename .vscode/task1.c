7e7643ce79fb4f310083940c441de80e55a4e692
Moosa Baig <am.moosabaig@gmail.com>
2026-09-03 17:00:04 +0500

    Task1 completed

---
+++ .vscode/task1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    char name[50];
+    int age;
+    char city[50];
+
+    printf("Enter your name: ");
+    fgets(name, 50, stdin);
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    getchar();
+
+    printf("Enter your city: ");
+    // scanf("%49s", city);
+    fgets(city, 50, stdin);
+
+    printf("Name: %s \n", name);
+    printf("Age: %d \n \n", age);
+    printf("City: %s \n", city);
+
+
+}
