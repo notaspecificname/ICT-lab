43a4380fb5ab1df033cb013fc138f5462bcad86c
Moosa Baig <am.moosabaig@gmail.com>
2026-09-03 18:15:51 +0500

    Removed executable file

---
+++ Lab 3/Task2/Task2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int num1;
+    int num2;
+
+    printf("Enter first number: ");
+    scanf("%d",&num1);
+    printf("Enter second num: ");
+    scanf("%d",&num2);
+
+    printf("First Number: %d \n", num1);
+    printf("Second Number: %d \n", num2);
+
+
+}
